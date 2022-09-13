8d169c18c581dd15de80d33ed18c13d17092b8ac
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-09-13 17:15:31 -0300

    Add a fix to Category

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -37,7 +37,6 @@ public class Category implements Serializable{
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
         return result;
     }
 
@@ -55,11 +54,8 @@ public class Category implements Serializable{
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (nome == null) {
-            if (other.nome != null)
-                return false;
-        } else if (!nome.equals(other.nome))
-            return false;
         return true;
     }
+
+    
 }
