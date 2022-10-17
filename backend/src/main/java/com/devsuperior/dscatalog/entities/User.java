41d75a51fb531ec49f50f028bf75fb49f0e80c7e
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-10-17 11:39:29 -0300

    Add user CRUD

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -19,12 +19,14 @@ public class User implements Serializable{
     private String email;
     private String password;
     
-    @ManyToMany
+    //fetch garante que os dados de perfis (roles) serão carregados junto com o usuário 
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "tb_user_role",
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+        //Não permite elementos repetidos
+        private Set<Role> roles = new HashSet<>();
     
     public User() {
     }
