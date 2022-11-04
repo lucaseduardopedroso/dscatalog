0597f5f85aad6b2ef646d74020b86f1b6dd6df57
Lucas Pedroso <lucaseduardosilvapedroso@gmail.com>
2022-11-04 11:12:48 -0300

    Add H2 config

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,10 +17,13 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
     
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore tokenStore;
     
     //Public endpoints (free access)
-    private static final String[] PUBLIC = { "/oauth/token" };
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
     //Operator or Admin endpoints
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
     //Admin endpoints
@@ -30,6 +36,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Enable H2 frames
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
             //Authorize PUBLIC endpoints (No login needed)
             .antMatchers(PUBLIC).permitAll()
